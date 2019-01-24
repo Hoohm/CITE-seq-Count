06942574aa5e38a15e7a42915cecce55ce69cefc
Patrick Roelli <patrick.roelli@gmail.com>
2019-01-24 01:32:28 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="CITE-seq-Count",
-    version="1.4.0",
+    version="1.4.1",
     author="Roelli Patrick",
     author_email="patrick.roelli@gmail.com",
     description="A python package that counts CITE seq or hashing data for single cell experiments",
