059e2d0695d50e9551dbfed54ace502f8095965d
Tobias Krause <tobiaspk1@gmail.com>
2025-04-22 10:22:36 -0400

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="CITE-seq-Count",
-    version="1.4.5",
+    version="1.4.5.pr.197",
     author="Roelli Patrick",
     author_email="patrick.roelli@gmail.com",
     description="A python package to map reads from CITE-seq or hashing data for single cell experiments",
